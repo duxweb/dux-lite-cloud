049aba7d86116523fe69726c652b73f84a6cfa4d
Mr.L <admin@duxphp.com>
2025-06-26 13:53:51 +0800

    update

--- src/Command/UpdateCommand.php
+++ src/Command/UpdateCommand.php
@@ -10,7 +10,6 @@ use Nette\Utils\FileSystem;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
-use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 
 class UpdateCommand extends Command
