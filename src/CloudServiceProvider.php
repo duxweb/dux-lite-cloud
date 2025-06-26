9575390fc970a6aa9b9cd59d79278f11bc6eef56
Mr.L <admin@duxphp.com>
2025-06-26 15:18:30 +0800

    修复 CloudServiceProvider.php 中的命令注册逻辑，将命令添加的 foreach 循环移至 boot 方法中，以确保在启动时正确注册命令。

--- src/CloudServiceProvider.php
+++ src/CloudServiceProvider.php
@@ -33,13 +33,13 @@ class CloudServiceProvider implements PluginProvider
 
     public static function register(Bootstrap $bootstrap): void
     {
-        foreach (self::getCommands() as $command) {
-            $bootstrap->command->add(new $command());
-        }
     }
 
     public static function boot(Bootstrap $bootstrap): void
     {
+        foreach (self::getCommands() as $command) {
+            $bootstrap->command->add(new $command());
+        }
     }
 
     public static function apps(): array
